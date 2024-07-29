0847361b805c7bb3ae8f496441b089c10a7deddd
Evelina <evelina.tev@mail.ru>
2024-07-29 14:33:48 +0300

    Stub and tests for findByNumber

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -5,6 +5,15 @@ public class PhoneBookTest {
     @Test
     public void testAdd() {
         PhoneBook phoneBook = new PhoneBook();
-        assertEquals(0, phoneBook.add("John Doe", "555-1212"));
+//        assertEquals(0, phoneBook.add("John Doe", "555-1211"));
+        assertEquals(1, phoneBook.add("Jane Doe", "555-1213"));
+        assertEquals(1, phoneBook.add("Jane Doe", "555-1212"));
+    }
+
+    @Test
+    public void testFindByNumber() {
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("John Doe", "555-1212");
+        assertEquals("John Doe", phoneBook.findByNumber("555-1212"));
     }
 }
