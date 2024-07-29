ad9b2043f1cf6188f3f7acf3e03f44189a5977b0
Evelina <evelina.tev@mail.ru>
2024-07-29 14:29:56 +0300

    add implemented

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class PhoneBookTest {
+    @Test
+    public void testAdd() {
+        PhoneBook phoneBook = new PhoneBook();
+        assertEquals(0, phoneBook.add("John Doe", "555-1212"));
+    }
 }
